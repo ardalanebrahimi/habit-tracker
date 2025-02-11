bc14a11b93c401a75ade4170d7da9ba1e18d09f7
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2025-02-12 00:30:49 +0100

    improve add design, burger menu, pages headder, and components design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,24 @@ export class AppComponent {
   menuOpen = false;
 
   constructor(public authService: AuthService, private router: Router) {}
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
+    this.closeMenu();
+  }
+
+  // Close menu when pressing the Escape key
+  @HostListener('document:keydown.escape', [])
+  onEscPress() {
+    this.closeMenu();
   }
 }
